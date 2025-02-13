b2eb16b2d6faec3587146d5d5a56ccec29f6e405
Park Yoonseo <patpark97@gmail.com>
2025-02-13 16:07:41 +0900

    feat: 게시판 메뉴 추가

---
+++ src/pages/Board.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import BoardForm from "../components/BoardForm";
+import BoardList from "../components/BoardList";
+import { BoardItem } from "../types/board";
+
+const Board = () => {
+  const [items, setItems] = useState<BoardItem[]>([]);
+  const [idCounter, setIdCounter] = useState(1);
+
+  const addItem = (item: Omit<BoardItem, "id">) => {
+    setItems([...items, { id: idCounter, ...item }]);
+    setIdCounter(idCounter + 1);
+  };
+
+  const deleteItem = (id: number) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <h1 className="my-4">게시판</h1>
+      <BoardForm onSubmit={addItem} />
+      <BoardList items={items} onDelete={deleteItem} />
+    </div>
+  );
+};
+
+export default Board;
