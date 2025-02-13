5a833d3707b0599c2218fb88ef0a2a4177d856c8
Park Yoonseo <patpark97@gmail.com>
2025-02-13 16:06:36 +0900

    feat: 네비게이션바 추가 및 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import Board from "./pages/Board";
+import "./App.css";
+import "./styles/Home.css"
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        {/* 네비게이션 바 */}
+        <nav className="navbar">
+          <div className="nav-container">
+            <Link to="/" className="logo">
+              Yoonseo Park
+            </Link>
+            <ul className="nav-links">
+              <li>
+                <Link to="/board">방명록</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        {/* 페이지 라우트 설정 */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/board" element={<Board />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
